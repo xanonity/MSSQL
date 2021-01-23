f4d83abd2ed9f9cec2f66001b99735b06c94ef1e
xanonity <31986856+xanonity@users.noreply.github.com>
2021-01-23 17:33:54 +0300

    Update СписокТрассировок.sql

--- СписокТрассировок.sql
+++ СписокТрассировок.sql
@@ -1,13 +1,13 @@
-SELECT * FROM sys.fn_trace_getinfo(0) -- получить все трассы
+SELECT * FROM sys.fn_trace_getinfo(0) -- РїРѕР»СѓС‡РёС‚СЊ РІСЃРµ С‚СЂР°СЃСЃС‹
 
-SELECT * FROM sys.fn_trace_getinfo(3) -- получить трассу с ID = 3
+SELECT * FROM sys.fn_trace_getinfo(3) -- РїРѕР»СѓС‡РёС‚СЊ С‚СЂР°СЃСЃСѓ СЃ ID = 3
 
-exec sp_trace_setstatus 3, 1 -- включить трассу с ID = 3
-exec sp_trace_setstatus 3, 0 -- выключить трассу с ID = 3
+exec sp_trace_setstatus 3, 1 -- РІРєР»СЋС‡РёС‚СЊ С‚СЂР°СЃСЃСѓ СЃ ID = 3
+exec sp_trace_setstatus 3, 0 -- РІС‹РєР»СЋС‡РёС‚СЊ С‚СЂР°СЃСЃСѓ СЃ ID = 3
 
 
 
-select * from sys.dm_xe_sessions -- посмотреть трассировки Extended events
+select * from sys.dm_xe_sessions -- РїРѕСЃРјРѕС‚СЂРµС‚СЊ С‚СЂР°СЃСЃРёСЂРѕРІРєРё Extended events
 
-ALTER EVENT SESSION [session_name] ON SERVER  STATE = START -- запустить трассу Extended events 
-ALTER EVENT SESSION [session_name] ON SERVER  STATE = STOP -- остановить трассу Extended events
+ALTER EVENT SESSION [session_name] ON SERVER  STATE = START -- Р·Р°РїСѓСЃС‚РёС‚СЊ С‚СЂР°СЃСЃСѓ Extended events 
+ALTER EVENT SESSION [session_name] ON SERVER  STATE = STOP -- РѕСЃС‚Р°РЅРѕРІРёС‚СЊ С‚СЂР°СЃСЃСѓ Extended events
